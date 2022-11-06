bfceb71cec921c5a4db2f62a2782ece9e3d09a7f
Axel Peralta <76273740+Kurosios@users.noreply.github.com>
2022-11-06 18:06:43 -0300

    Update Logica.js

--- Logica.js
+++ Logica.js
@@ -1,32 +1,58 @@
 let Bolita = document.querySelector(".punto");
-let puntos = 0;
-
+let Stop = document.querySelector(".stop");
+let faltan = document.querySelector(".faltan")
 let resetear = document.querySelector(".reset");
 let total = document.querySelector(".score");
-
+let start = document.querySelector(".start");
+let puntos = 0;
+let b = 60;
+let num = 1;
+var dk;
+var plus;
+var pato;
+tiempo =1000;
 Bolita = Bolita.addEventListener("click",sumar);
+Stop = Stop.addEventListener("click",detener);
 resetear = resetear.addEventListener("click",borrar);
+start = start.addEventListener("click",comenzar);
+
+
 
 total.innerHTML = "Puntuacion: " + puntos;
+faltan.innerHTML = "Quedan: " + b + " segundos restantes";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-var t ;
-let tiempo = 3000;
-function comenzar() {
- t = setInterval(mover, tiempo);
- 
+
+
+
+function comenzar () {
+   dk = setInterval(mover,tiempo);
+   pato = setInterval(descontador,1000);
+   plus = setInterval(detener,60000);
+   
 }
- 
-function stop() {
-   clearInterval(t);
- }
 
+function descontador (){
+   b = b - 1;
+   faltan.innerHTML = "Quedan: " + b + " segundos restantes";
+   if (b == 0) {
+      pato = clearInterval(pato);
+   } 
+}
 
-let b = 30;
+function aumentar () {
+   if ( tiempo > 250) {
+   clearInterval(dk);
+   tiempo = tiempo - 50;
+   dk = setInterval(mover,tiempo);}
+}
 
-
+function detener (){
+   dk = clearInterval(dk);
+   pato = clearInterval(pato);
+}
 function mover() {
    let hola = getRandomInt(200).toString() + "px";
    let chau = getRandomInt(200).toString() + "px";
@@ -37,16 +63,18 @@ function mover() {
 function sumar() {
    puntos = puntos + 1;
    total.innerHTML = "Puntuacion: " + puntos;
-   tiempo = tiempo - 300;
+   mover();
+   aumentar();
    console.log(tiempo);
 }
 
 function borrar() {
    puntos = 0;
-   console.log(puntos);
-   a = 30;
-   b = 30;
+   console.log("puntos: "+ puntos);
+   b = 60;
    total.innerHTML = "Puntuacion: " + puntos;
-   tiempo = 3000;
-   console.log(tiempo);
+   detener();
+   tiempo = 1000;
+   console.log("Tiempo: " + tiempo);
+
 }
