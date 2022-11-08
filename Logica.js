59df486573465b41c2cdf0856804f59d914a747c
Axel Peralta <76273740+Kurosios@users.noreply.github.com>
2022-11-07 21:30:51 -0300

    Update Logica.js

--- Logica.js
+++ Logica.js
@@ -1,11 +1,12 @@
 let Bolita = document.querySelector(".punto");
 let Stop = document.querySelector(".stop");
 let faltan = document.querySelector(".faltan")
+let max = document.querySelector(".max-punt")
 let resetear = document.querySelector(".reset");
 let total = document.querySelector(".score");
 let start = document.querySelector(".start");
 let puntos = 0;
-let b = 60;
+var b = 10;
 let num = 1;
 var dk;
 var plus;
@@ -16,10 +17,10 @@ Stop = Stop.addEventListener("click",detener);
 resetear = resetear.addEventListener("click",borrar);
 start = start.addEventListener("click",comenzar);
 
-
-
+let mp = 0;
+max.innerHTML = "la puntuacion maxima es: " + 0;
 total.innerHTML = "Puntuacion: " + puntos;
-faltan.innerHTML = "Quedan: " + b + " segundos restantes";
+faltan.innerHTML = "Restan: " + b + " segundos para que termine";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -30,16 +31,17 @@ function getRandomInt(max) {
 function comenzar () {
    dk = setInterval(mover,tiempo);
    pato = setInterval(descontador,1000);
-   plus = setInterval(detener,60000);
+   plus = setInterval(detener,b*1000);
    
 }
 
 function descontador (){
    b = b - 1;
-   faltan.innerHTML = "Quedan: " + b + " segundos restantes";
+   faltan.innerHTML = "Restan: " + b + " segundos para que termine";
    if (b == 0) {
       pato = clearInterval(pato);
    } 
+   console.log(b)
 }
 
 function aumentar () {
@@ -69,12 +71,18 @@ function sumar() {
 }
 
 function borrar() {
+   if(mp < puntos){
+      mp = puntos;
+   }
    puntos = 0;
    console.log("puntos: "+ puntos);
-   b = 60;
+   b = 10;
    total.innerHTML = "Puntuacion: " + puntos;
    detener();
    tiempo = 1000;
+   faltan.innerHTML = "Restan: " + b + " segundos para que termine";
    console.log("Tiempo: " + tiempo);
+   max.innerHTML = "la puntuacion maxima es: " + mp;
 
 }
+
